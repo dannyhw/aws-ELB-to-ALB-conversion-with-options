fdb2206823a34030b9c5d49c6b593a1d1443cda9
Danny <danny.h.williams@hotmail.com>
2016-10-27 13:26:25 +0100

    region as a bash variable

--- run_for_all.sh
+++ run_for_all.sh
@@ -1,6 +1,7 @@
 names=$(python ./print_all_elbs.py)
+region="--region eu-central-1"
 if [ "$1" == "--dry-run" ]; then
-    for name in $names; do ./copy_classic_load_balancer.py --name $name --register-targets --region eu-central-1 --dry-run; done
+    for name in $names; do ./copy_classic_load_balancer.py --name $name --register-targets $region --dry-run; done
 else
-    for name in $names; do ./copy_classic_load_balancer.py --name $name --register-targets --region eu-central-1; done
+    for name in $names; do ./copy_classic_load_balancer.py --name $name --register-targets $region; done    
 fi
